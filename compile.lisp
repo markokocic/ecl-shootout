c021ce0421b8585cdd2531a8caf829cba96e1cca
Marko Kocic <marko.kocic@gmail.com>
2009-06-23 18:54:46 +0200

    Add c::*cc-flags* to compile.

--- compile.lisp
+++ compile.lisp
@@ -1,6 +1,7 @@
 (require 'cmp)
 
 (defun create-exec (file)
-  (c::builder :program file
-              :lisp-files (list (compile-file file :system-p t))
-              :epilogue-code '(resume)))
+  (let ((c::*cc-flags* " -O3 -march=i686 -fomit-frame-pointer -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -fPIC -D_THREAD_SAFE  -Dlinux "))
+    (c::builder :program file
+                :lisp-files (list (compile-file file :system-p t))
+                :epilogue-code '(resume))))
